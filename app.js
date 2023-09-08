25390be84958104a8b6be35789e47ba7143a5e5a
Egbe Alexander <alexsucceeds@gmail.com>
2023-09-08 03:40:27 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// Step 1: Import required modules and create an Express app
+const express = require('express');
+const app = express();
+
+// Step 2: Define a route that handles GET requests with query parameters
+app.get('/api', (req, res) => {
+  // Step 3: Extract query parameters from the request URL
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  // Step 4: Get the current day of the week
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const currentDate = new Date();
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
+
+  // Step 5: Get the current UTC time within a +/-2 minute window
+  const currentUTCDate = new Date();
+  const currentUTCTime = currentUTCDate.toISOString();
+
+  // Step 6: Define GitHub URLs for file and repository
+  const githubFileURL = "https://github.com/username/repo/blob/main/file_name.ext";
+  const githubRepoURL = "https://github.com/username/repo";
+
+  // Step 7: Create the JSON response object
+  const jsonResponse = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: currentUTCTime,
+    track: track,
+    github_file_url: githubFileURL,
+    github_repo_url: githubRepoURL,
+    status_code: 200,
+  };
+
+  // Step 8: Send the JSON response as the HTTP response
+  res.json(jsonResponse);
+});
+
+// Step 9: Start the Express server and listen on a specified port
+const port = process.env.PORT || 4000; // Use the environment variable PORT or default to 3000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
